b44c85930114052c652b805c2e3fc5e7a1591962
alexander valdaitsevv <valdaitsevv@mail.ru>
2023-10-01 13:09:49 +0300

    Add full project

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+namespace TestTask.Services.Implementations;
+
+public class OrderService : IOrderService
+{
+    private readonly ApplicationDbContext _context;
+
+    public OrderService(ApplicationDbContext context) => _context = context;
+
+    public async Task<Order> GetOrder() =>
+        await _context.Orders.OrderByDescending(o => o.Price).FirstOrDefaultAsync();
+
+    public async Task<List<Order>> GetOrders() =>
+        await _context.Orders.Where(o => o.Quantity > 10).ToListAsync();
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+namespace TestTask.Services.Implementations;
+
+public class UserService : IUserService
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserService(ApplicationDbContext context) => _context = context;
+
+    public async Task<User> GetUser()
+    {
+        var userWithOrdersCount = await _context.Orders
+            .GroupBy(o => o.UserId)
+            .Select(u => new
+            {
+                UserId = u.Key,
+                OrdersCount = u.Count()
+            })
+            .OrderByDescending(u => u.OrdersCount)
+            .FirstOrDefaultAsync();
+
+        var user = await _context.Users
+            .Where(u => u.Id.Equals(userWithOrdersCount.UserId))
+            .FirstOrDefaultAsync();
+        
+        return user;
+    }
+
+    public async Task<List<User>> GetUsers() =>
+        await _context.Users
+            .Where(u => u.Status.Equals(UserStatus.Inactive))
+            .ToListAsync();
+}
